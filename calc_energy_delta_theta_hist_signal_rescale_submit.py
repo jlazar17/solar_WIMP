761c243675a4fdfaa1096a4a0a7738d98c45e019
jlazar17 <jeff.p.lazar@gmail.com>
2019-12-05 12:35:57 -0600

    add rescaling scripts

---
+++ calc_energy_delta_theta_hist_signal_rescale_submit.py
@@ -0,0 +1,32 @@
+import pycondor
+import numpy as np
+from sys import argv as args
+
+ch = int(args[1])
+m  = int(args[2])
+nu_types = ["nu", "nuBar"]
+
+path   = "/home/jlazar/condor_logs/e_d_theta_signal_rescale"
+error  = "%s/error" % path
+output = "%s/output" % path
+log    = "%s/log" % path
+submit = "%s/submit" % path
+
+run_ns = [ int(i) for i in np.linspace(0,99,100) ]
+mc_file = "/data/user/jlazar/solar_WIMP/data/mcRecarray.npy"
+
+
+for nt in nu_types:
+    outfile = "ch%s_m%s_%s_energy_delta_theta_hist_signal.out" % (ch, m, nt)
+    run     = pycondor.Job("ch%s_m%s_%s_energy_delta_theta_hist_signal" % (ch, m, nt), 
+               "/data/user/jlazar/solar_WIMP/get_energy_delta_theta_hist_signal.sh", 
+               error=error, 
+               output=output, 
+               log=log, 
+               submit=submit, 
+               universe="vanilla", 
+               notification="never"
+              )
+    for n in run_ns:
+        run.add_arg("%s %s %s %s" % (n, nt, ch, m))
+    run.build()
---
+++ get_energy_delta_theta_hist_signal_rescale.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+RUNN=$1
+NUTYPE=$2
+CH=$3
+MCHI=$4
+
+#eval `/cvmfs/icecube.opensciencegrid.org/py3-v4/setup.sh` python /data/user/jlazar/solar_WIMP/calc_energy_delta_theta_hist_signal.py ${RUNN} ${NUTYPE} ${CH} ${MCHI} ${MCFILE} | tail -1 >> ${OUTFILE}
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4/setup.sh` python /data/user/jlazar/solar_WIMP/calc_energy_delta_theta_hist_signal_rescale.py ${RUNN} ${NUTYPE} ${CH} ${MCHI}
